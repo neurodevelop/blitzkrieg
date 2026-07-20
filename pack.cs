493804bf9df121c65b49324fc27f294059010b2b
NeuDev <neudevelop@gmail.com>
2026-07-20 19:06:36 +0300

    JSON DTO for saving text, grid, and language.

---
+++ pack.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace blitz
+{
+    public class pack
+    {
+        public string text { get; set; }
+        public List<stage> grid { get; set; }
+        public string lang { get; set; }
+    }
+}
